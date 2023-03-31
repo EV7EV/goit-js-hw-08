92dc31df8bd65baeada8adbab21d13f927deaf91
EV7EV <ev@kneu.edu.ua>
2023-03-31 14:41:23 +0300

    finish h/w -8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,77 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(saveFormInput, 500));
+form.addEventListener('submit', submitForm);
+
+const storageFormKey = 'feedback-form-state';
+
+let info = JSON.parse(localStorage.getItem(storageFormKey)) || {};
+// Деструктуризація форми, для того щоб витягнути елементи , message
+const { email, message } = form.elements;
+
+reloadPage();
+
+function saveFormInput() {
+  info = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem(storageFormKey, JSON.stringify(info));
+}
+
+function reloadPage() {
+  if (info) {
+    email.value = info.email || '';
+    message.value = info.message || '';
+  }
+}
+
+function submitForm(evt) {
+  evt.preventDefault();
+  if (email.value === '' || message.value === '') {
+    return alert(`Заповніть всі поля`);
+  }
+  console.log({ email: email.value, message: message.value });
+  localStorage.removeItem(storageFormKey);
+  form.reset();
+}
+
+// const form = document.querySelector('.feedback-form');
+
+// form.addEventListener('input', throttle(saveFormInput, 500));
+// form.addEventListener('submit', submitForm);
+
+// const storageFormKey = 'feedback-form-state';
+
+// const date = JSON.parse(localStorage.getItem(storageFormKey)) || {};
+// // Деструктуризація форми, для того щоб витягнути елементи , message
+// const { email, message } = form.elements;
+
+// reloadPage();
+
+// function saveFormInput() {
+//   const info = {
+//     email: email.value,
+//     message: message.value,
+//   };
+//   localStorage.setItem(storageFormKey, JSON.stringify(info));
+// }
+
+// function reloadPage() {
+//   if (date) {
+//     email.value = date.email || '';
+//     message.value = date.message || '';
+//   }
+// }
+
+// function submitForm(evt) {
+//   evt.preventDefault();
+//   if (email.value === '' || message.value === '') {
+//     return alert(`Заповніть всі поля`);
+//   }
+//   console.log({ email: email.value, message: message.value });
+//   localStorage.removeItem(storageFormKey);
+//   form.reset();
+// }
